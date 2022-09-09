178597ab83e8a82e5c92d288634803b081277b3c
ziro37 <111128465+Ziro37@users.noreply.github.com>
2022-09-09 14:25:40 -0700

    Add files via upload

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/** 
+
+* main- print in the puts function
+
+*
+
+*desciption :main function 
+
+* this program prints all sentence within the quotation mark
+
+* Return : 0
+
+*/
+
+int main(void)
+
+{
+
+  char c;
+
+  int i;
+
+  long li;
+
+  long long int lli;
+
+   float f;
+
+printf("Size of a char :%d byte(s)\n" , sizeof(c));
+
+printf("Size of an int :%d byte(s)\n" , sizeof(i));
+
+printf("Size of a long int :%d byte(s)\n" , sizeof(li));
+
+printf("Size of a long long int :%d byte(s)\n" , sizeof(lli));
+
+printf("Size of a float :%d byte(s)\n" , sizeof(f));
+
+return(0);
+
+}
